380c5882e7db2e75077d5c1e2d1cf0052c78c810
narekkeryan <narek.keryan@gmail.com>
2020-04-05 00:30:45 +0400

    feat(root): initialization & configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# naves
+
+[![Build Status](https://travis-ci.org/narekkeryan/naves.svg)](https://travis-ci.org/narekkeryan/naves)
+
+## Powerful JavaScript Utility Library.
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
